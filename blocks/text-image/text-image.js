f69a315f39c7f3af69e95bbea02fca8aeb3e5e1f
Harish-DV <163002991+Harish-DV@users.noreply.github.com>
2024-03-20 16:08:14 +0100

    Updates bg color on text-image.js

--- blocks/text-image/text-image.js
+++ blocks/text-image/text-image.js
@@ -5,7 +5,7 @@ export function generateTextImageDOM(props) {
 
   // Build DOM
   const textImageDOM = document.createRange().createContextualFragment(`
-    <div style="height: 350px; background-color: purple;">
+    <div style="height: 350px; background-color: gray;">
         <div class='background' style='float:${imagePosition.innerHTML}'>${picture ? picture.outerHTML : ''}</div>
         <div class='foreground'>
           <div class='text'>
