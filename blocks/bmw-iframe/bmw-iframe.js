56719ff4dd220873c2257efc239dbee682287566
Amol <amol.y.patil@accenyure.com>
2024-04-02 13:29:43 +0530

    Removed height and width as per US specification

--- blocks/bmw-iframe/bmw-iframe.js
+++ blocks/bmw-iframe/bmw-iframe.js
@@ -1,11 +1,11 @@
 export function generateIFrameDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
-  const [iFrameUrl, iframeHeight, iframeWidth] = props;
+  const [iFrameUrl] = props;
   // const picture = pictureContainer.querySelector('picture');
   // Build DOM
   const iFrameDOM = document.createRange().createContextualFragment(`
     <div>
-    <iframe src="${iFrameUrl.textContent}" width="${iframeWidth.innerHTML}" height="${iframeHeight.innerHTML}" style="border:1px solid black;">
+    <iframe src="${iFrameUrl.textContent}" style="border:1px solid black;">
     </iframe>
     
     </div>
