cc850ac871e39706aaeed6707b894264f15345b0
Abhilash Mohapatra <abhilash.a.mohapatra@accenture.com>
2024-04-08 13:51:17 +0530

    iframe-73

--- blocks/bmw-iframe/bmw-iframe.js
+++ blocks/bmw-iframe/bmw-iframe.js
@@ -4,24 +4,23 @@ export function generateIFrameDOM(props) {
   // const picture = pictureContainer.querySelector('picture');
   // Build DOM
   const iFrameDOM = document.createRange().createContextualFragment(`
-    <div>
+    <div class="iframe-container">
     <iframe src="${iFrameUrl.textContent}" style="border:1px solid black;" id="bmwIframe">
     </iframe>
+    <div class="loader"></div>
     
     </div>
   `);
   return iFrameDOM;
 }
+const iframe = document.getElementById('bmwIframe');
+const loading = document.querySelector('.loader');
 
-// window.onload = function() {
-//   var iframe = document.getElementById('bmwIframe');
-//   var loadingOverlay = document.querySelector('.loading-overlay');
+iframe.addEventListener('load', () => {
+  loading.style.display = 'none';
 
-//   iframe.onload = function() {
-//     loadingOverlay.style.display = 'none';
-//   };
-// };
-
+  iframe.style.opacity = 1;
+});
 export default function decorate(block) {
   // get the first and only cell from each row
   const props = [...block.children].map((row) => row.firstElementChild);
