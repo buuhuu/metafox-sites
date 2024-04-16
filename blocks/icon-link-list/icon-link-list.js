ee955f78d140bf36f16e98a2e8dd8bcf93c1475e
bharath-yathiraj <bharath.yathiraj@accenture.com>
2024-04-16 20:49:45 +0530

    lint error fixes

--- blocks/icon-link-list/icon-link-list.js
+++ blocks/icon-link-list/icon-link-list.js
@@ -1,26 +1,24 @@
 import { generateIconDOM } from '../icon/icon.js';
 
-export default function decorate(block) {    
-    // the panels container
-    const panelContainer = document.createElement('div');
-    panelContainer.classList.add('icon-link-list-container');
-  
-    // get all children elements
-    const panels = [...block.children];
-  
-    // loop through all children blocks
-    [...panels].forEach((panel, i) => {
-      // generate the  panel
-      const [iconType,...rest] = panel.children;
-      const classesText = iconType.textContent.trim();
-      const iconDOM = generateIconDOM([iconType,...rest], classesText);
-          
-      panel.textContent = '';
-      panel.classList.add('icon','block',classesText);
-      panel.append(iconDOM);
-      panelContainer.append(panel);
-    });
-  
-    block.textContent = '';
-    block.append(panelContainer);
-  }
+export default function decorate(block) {
+  const panelContainer = document.createElement('div');
+  panelContainer.classList.add('icon-link-list-container');
+
+  // get all children elements
+  const panels = [...block.children];
+
+  // loop through all children blocks
+  [...panels].forEach((panel) => {
+    // generate the  panel
+    const [iconType, ...rest] = panel.children;
+    const classesText = iconType.textContent.trim();
+    const iconDOM = generateIconDOM([iconType, ...rest]);
+
+    panel.textContent = '';
+    panel.classList.add('icon', 'block', classesText);
+    panel.append(iconDOM);
+    panelContainer.append(panel);
+  });
+  block.textContent = '';
+  block.append(panelContainer);
+}
