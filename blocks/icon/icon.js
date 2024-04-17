0369ed6805f2a843b894839004bc32bcff812eb2
Joydeep Roy <j.d.roy@accenture.com>
2024-04-17 17:35:05 +0300

    js, css change for location, search icon

--- blocks/icon/icon.js
+++ blocks/icon/icon.js
@@ -1,22 +1,33 @@
 const iconList = {
-  facebook: '/icons/facebook.png',
-  instagram: '/icons/instagram.png',
-  youtube: '/icons/youtube.png',
-  twitter: '/icons/X-Logo.png',
-  linkedin: '/icons/linkedin.png',
-  location:'/icons/location.svg',
-  search:'/icons/search.svg'
+  facebook: "/icons/facebook.png",
+  instagram: "/icons/instagram.png",
+  youtube: "/icons/youtube.png",
+  twitter: "/icons/X-Logo.png",
+  linkedin: "/icons/linkedin.png",
 };
+
+const iconListArray = ["location", "search"];
 
 export function generateIconDOM(props) {
   // Extract properties, always same order as in model, empty string if not set
   const [iconType, altText, iconLink] = props;
-  const iconPath = iconList[iconType.textContent];
-  // Build DOM
-  const iconDom = document.createRange().createContextualFragment(`
+  let iconPath;
+  let iconDom;
+  if (iconListArray.includes(iconType.textContent)) {
+    // Build DOM
+    iconDom = document.createRange().createContextualFragment(`
+      <a class="icon-container" href="${iconLink.textContent}">
+        <span class="${iconType.textContent}"><i></i></span>
+      </a>`);
+  } else {
+    iconPath = iconList[iconType.textContent];
+    // Build DOM
+    iconDom = document.createRange().createContextualFragment(`
     <a class="icon-container" href="${iconLink.textContent}">
     <img src="${iconPath}" alt="${altText.textContent}"/>
     </a>`);
+  }
+
   return iconDom;
 }
 
@@ -24,6 +35,6 @@ export default function decorate(block) {
   // get the first and only cell from each row
   const props = [...block.children].map((row) => row.firstElementChild);
   const iconDOM = generateIconDOM(props);
-  block.textContent = '';
+  block.textContent = "";
   block.append(iconDOM);
 }
