b61477d65b56e009471bdde7729fa06f556bd3e5
Amol <amol.y.patil@accenyure.com>
2024-04-20 14:47:57 +0530

    updated menu teaser files
    updated menu teaser files

---
+++ blocks/menu-teaser/menu-teaser.js
@@ -0,0 +1,29 @@
+export function generateMenuTeaserDOM(props) {
+  // Extract properties, always same order as in model, empty string if not set
+  const [tesaserimage, altText, richtext, LinkLabel, LinkTitle, Link, LinkType] = props;
+  //   const picture = pictureContainer.querySelector('picture');
+  //   const image = picture.querySelector('image');
+  //   if (Boolean(image) && Boolean(altText)) {
+  //     image.alt = altText;
+  //   }
+
+  console.log('all values coming in menu teaser', tesaserimage, altText, richtext, LinkLabel, LinkTitle, Link, LinkType);
+
+  // Build DOM
+  const menuteaserDOM = document.createRange().createContextualFragment(`
+      <div>
+          ${tesaserimage}, ${altText}, ${richtext}, ${LinkLabel}, ${LinkTitle}, ${Link}, ${LinkType}
+      </div>
+    `);
+
+  // add final teaser DOM and classes if used as child component
+  return menuteaserDOM;
+}
+
+export default function decorate(block) {
+  // get the first and only cell from each row
+  const props = [...block.children].map((row) => row.firstElementChild);
+  const menuteaserDOM = generateMenuTeaserDOM(props);
+  block.textContent = '';
+  block.append(menuteaserDOM);
+}
